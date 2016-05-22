e7ca782980f3f382bda1be5d1c63a603231c208e
pavanpc <pcpavan.471@gmail.com>
2016-05-23 01:15:49 +0530

    added docker file

--- kafka_setup.sh
+++ kafka_setup.sh
@@ -1,5 +1,7 @@
 #add below line if you are using virtual box, dev2 is the virtual environment name
-#eval "$(docker-machine env dev2)"
+eval "$(docker-machine env dev2)"
+
+docker build -f Dockerfile_kafka .
 
 #brings the kafka image present in docker_compose.yml up 
 docker-compose up -d
@@ -7,6 +9,8 @@ docker-compose up -d
 # copies the fat jar and config required to publish twitter events to kafka
 docker cp twitter-feed-0.0.1-SNAPSHOT-jar-with-dependencies.jar kafka:/home/twitter-feed-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 docker cp AppConfig.properties kafka:/home/AppConfig.properties
+
+docker exec -it $(docker-compose ps -q kafka) bash -c " kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic twitter-topic"
 
 # executes below command inside the kafka running container and outputs the result to stdout. You can see twitter messages on console
 docker exec -it $(docker-compose ps -q kafka) bash -c "cd /home/ && java -jar twitter-feed-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
--- spark_setup.sh
+++ spark_setup.sh
@@ -1,11 +1,13 @@
 #Uncomment below line if you are using virtual box, dev2 is the virtual environment name
 #eval "$(docker-machine env dev2)"
 
-#brings the spark image present in docker-compose.yml up and links it with the kafka image running
-docker-compose up -d
+docker build -f Dockerfile .
 
 # builds the new docker image for spark using Dockerfile present in current directory
-docker build --no-cache -t jupyter/all-spark-notebook:v3 .
+docker build -t jupyter/all-spark-notebook:v3 .
+
+#brings the spark image present in docker-compose.yml up and links it with the kafka image running
+docker-compose up -d
 
 #brings the spark container up and executes below command(starts spark streaming application) inside container and ouputs to stdout
 docker run -it jupyter/all-spark-notebook:v3 /usr/local/spark/bin/spark-submit  --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.1  Twitter-Feeds-Stream-Clustering/pyspark-streaming-clustering/streaming_k_means.py
